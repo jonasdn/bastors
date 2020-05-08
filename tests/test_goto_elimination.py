f191f12483ad8d1519e0d7a111184f14bea1f148
Jonas Danielsson <jonas@threetimestwo.org>
2020-05-08 20:04:44 +0200

    Add test case for algo 2.1
    This will not pass since it is not implemented.

--- tests/test_goto_elimination.py
+++ tests/test_goto_elimination.py
@@ -77,6 +77,104 @@ class TestGotoELim(unittest.TestCase):
         """
         self.__assert_output("1.2.bas", b"24450\n")
 
+    def test_2_1(self):
+        """
+        GOTO occurs in some parent block (> 1) of the block where the label is
+        contained in and goto occurs before the label.
+
+        This cannot be constructed by a TinyBasic program since our TinyBasic
+        does not allow multi-statement then. It can occur as part of goto
+        elimination though. So we construct an example here.
+        """
+        statements = dict()
+        statements["main"] = [
+            parse.Let(None, parse.VariableExpression("a"), 0),
+            parse.Let(None, parse.VariableExpression("b"), 2),
+            parse.Let(None, parse.VariableExpression("d"), 78),
+            parse.If(
+                None,
+                [
+                    parse.Condition(
+                        parse.VariableExpression("a"),
+                        ">=",
+                        0,
+                        parse.ConditionEnum.INITIAL,
+                    )
+                ],
+                [
+                    parse.Print(None, [parse.VariableExpression("a")]),
+                    parse.If(
+                        None,
+                        [
+                            parse.Condition(
+                                parse.VariableExpression("b"),
+                                "!=",
+                                2,
+                                parse.ConditionEnum.INITIAL,
+                            )
+                        ],
+                        [parse.Goto(None, 20)],
+                    ),
+                    parse.Let(None, parse.VariableExpression("a"), 4),
+                    parse.If(
+                        None,
+                        [
+                            parse.Condition(
+                                parse.VariableExpression("a"),
+                                ">",
+                                0,
+                                parse.ConditionEnum.INITIAL,
+                            )
+                        ],
+                        [
+                            parse.Print(None, [parse.VariableExpression("a")]),
+                            parse.Let(None, parse.VariableExpression("c"), 7),
+                            parse.If(
+                                None,
+                                [
+                                    parse.Condition(
+                                        parse.VariableExpression("c"),
+                                        "=",
+                                        5,
+                                        parse.ConditionEnum.INITIAL,
+                                    )
+                                ],
+                                [
+                                    parse.Let(None, parse.VariableExpression("b"), 2),
+                                    parse.Print(None, [parse.VariableExpression("c")]),
+                                    parse.Let(20, parse.VariableExpression("d"), 32),
+                                    parse.Let(None, parse.VariableExpression("e"), 5),
+                                ],
+                            ),
+                            parse.Let(
+                                None,
+                                parse.VariableExpression("a"),
+                                parse.VariableExpression("d"),
+                            ),
+                            parse.Print(None, [parse.VariableExpression("a")]),
+                        ],
+                    ),
+                    parse.Let(None, parse.VariableExpression("b"), 2),
+                    parse.Let(None, parse.VariableExpression("a"), 2),
+                    parse.Let(None, parse.VariableExpression("e"), 2),
+                ],
+            ),
+            parse.End(None),
+        ]
+        program = parse.Program(statements)
+        self.assertEqual(classify_goto(program), "2.1")
+
+        out = open("2_1.rs", "w")
+        rust = Rustify()
+        rust.visit(eliminate_goto(program))
+        rust.output(out)
+        out.close()
+
+        process = subprocess.call(["rustc", "2_1.rs"], subprocess.PIPE)
+        process = subprocess.Popen(["./2_1"], stdout=subprocess.PIPE)
+        (program_output, _) = process.communicate()
+        self.assertEqual(b"0\n4\n78\n", program_output)
+
     def test_3_1(self):
         """
         Label occurs in some parent block (> 1) of the block where the goto is
@@ -152,7 +250,7 @@ class TestGotoELim(unittest.TestCase):
         self.assertEqual(b"1\n1\n5\n7\n", program_output)
 
     def tearDown(self):
-        globs = [u"1_1_bas*", u"1_1_bare_bas*", u"1_2_bas*", u"3_1*"]
+        globs = [u"1_1_bas*", u"1_1_bare_bas*", u"1_2_bas*", u"2_1", u"3_1*"]
         for g in globs:
             for file in glob.glob(g):
                 os.unlink(os.path.abspath(file))
